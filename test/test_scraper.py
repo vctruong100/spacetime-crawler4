f0c0da2f7ae85a0d17789c1aeeedf0c6f8cbf0de
VcTruong <vctruong100@gmail.com>
2024-04-15 18:27:53 -0700

    test/test_scraper.py: fix test functions, add new tests

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -13,25 +13,26 @@ class TestIsValid(unittest.TestCase):
         self.assertTrue(is_valid("https://www.stat.uci.edu/"))
 
     def test_invalid(self):
-        self.assertFalse(is_valid("ftp://www.ics.uci.edu/"))
         self.assertFalse(is_valid("http://www.uci.edu/"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.html"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.pdf"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.txt"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.doc"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.docx"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.ppt"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.pptx"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.xls"))
-        self.assertFalse(is_valid("http://www.ics.uci.edu/~lopes/teaching/inf141/project/requirements.xlsx"))
+        self.assertFalse(is_valid("https://www.unrelated.com/"))
+        self.assertFalse(is_valid("ftp://www.uci.edu/"))
+    
+    def test_invalid_extensions(self):
+        self.assertFalse(is_valid("http://www.ics.uci.edu/image.png"))
+        self.assertFalse(is_valid("http://www.ics.uci.edu/image.jpg"))
+        self.assertFalse(is_valid("http://www.ics.uci.edu/image.jpeg"))
+        self.assertFalse(is_valid("http://www.ics.uci.edu/image.gif"))
 
     def test_fragment_removal(self):
-        self.assertTrue(is_valid("http://www.ics.uci.edu/#fragment"))
-        self.assertTrue(is_valid("http://www.ics.uci.edu/#fragment1#fragment2"))
+        self.assertTrue(is_valid("http://www.ics.uci.edu/#fragment" == "http://www.ics.uci.edu/"))
+
+    def test_subdomains(self):
+        self.assertTrue(is_valid("http://subdomain.ics.uci.edu/"))
+        self.assertTrue(is_valid("http://subdomain.cs.uci.edu/"))
+        self.assertTrue(is_valid("http://subdomain.informatics.uci.edu/"))
+        self.assertTrue(is_valid("http://subdomain.stat.uci.edu/"))
+        self.assertFalse(is_valid("http://subdomain.unrelated.com/"))
+    
 
     
 if __name__ == "__main__":
