9a8d29e01e086cb5550ad1cec3d262f4fed72d81
Jason Vu <jasondv2@uci.edu>
2024-04-18 13:15:08 -0700

    helpers/stopwords_set: Add is_stopword
    Adding is_stopword just to unify with helpers/contra_set, which likewise exposes the
    is_contraction function.

--- helpers/stopwords_set.py
+++ helpers/stopwords_set.py
@@ -1,10 +1,23 @@
 # helpers/stopwords_set.py
 #
-# set for rsrc/stopwords.txt
+# word set for rsrc/stopwords.txt
+# check if a particular word is a stopword or not
 
 STOPWORDS_SET = set()
 
-# load stopwords into STOPWORDS_SET
+# open rsrc/stopwords.txt
+# load words into STOPWORDS_SET
 with open("rsrc/stopwords.txt", "r") as infile:
     for word in infile:
         STOPWORDS_SET.add(word.strip())
+
+def is_stopword(word):
+    """Returns True if `word` is a stopword according to
+    rsrc/stopwords.txt. Returns False otherwise.
+
+
+    :param word str: The string
+    :return: Whether it's a stopword
+    :rtype: bool
+    """
+    return word in STOPWORDS_SET
