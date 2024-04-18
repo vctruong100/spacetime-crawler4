d37bdef2f1f6c9c5eef52140b795cfe601421a56
Jason Vu <jasondv2@uci.edu>
2024-04-18 00:55:18 -0700

    helpers/stopwords_set: Add STOPWORDS_SET and related tests

---
+++ helpers/stopwords_set.py
@@ -0,0 +1,10 @@
+# helpers/stopwords_set.py
+#
+# set for rsrc/stopwords_set.py
+
+STOPWORDS_SET = set()
+
+# load stopwords into STOPWORDS_SET
+with open("rsrc/stopwords.txt", "r") as infile:
+   for word in infile:
+      STOPWORDS_SET.add(word.strip())
---
+++ test/test_stopwords.py
@@ -0,0 +1,11 @@
+import unittest
+from helpers.stopwords_set import STOPWORDS_SET
+
+class TestStopwords(unittest.TestCase):
+   def test_in_stopwords(self):
+      self.assertTrue("don't" in STOPWORDS_SET)
+      self.assertTrue("hello" not in STOPWORDS_SET)
+
+
+if __name__ == "__main__":
+   unittest.main()
