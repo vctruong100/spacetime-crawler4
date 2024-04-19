488194080f9f158f984e0feb0e0285452123af9c
Jason Vu <jasondv2@uci.edu>
2024-04-18 17:09:54 -0700

    helpers/word_count: Add word_count

---
+++ helpers/word_count.py
@@ -0,0 +1,28 @@
+# helpers/word_counts.py
+#
+# computes the word frequencies of the response
+# based on the tokenizer in helpers/tokenize
+
+from helpers.tokenize import tokenize
+from helpers.page_cache import parse_response
+
+def word_count(url, resp):
+    """Aggregates the tokens after tokenizing the response page.
+    See helpers/tokenize for more info on how the tokenizer works,
+    and what tokens are returned).
+
+    Returns a mapping of tokens/words to its frequency.
+
+    :param url str: The URL
+    :param resp Response: The response of the URL
+    :return: The mapping of words to its frequency
+    :rtype: dict[str, int]
+    """
+    word_dict = dict()
+    parsed = parse_response(url, resp)
+
+    for text in text_content:
+        tokens = tokenize(text)
+        for token in tokens:
+            word_dict[token] = word_dict.get(token, 0) + 1
+    return word_dict
