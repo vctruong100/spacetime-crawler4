bcb3548dd0b3eeab733f4ae0fd32b2b8b25ada01
VcTruong <vctruong100@gmail.com>
2024-04-19 18:32:35 -0700

    utils/response: add re_direct

--- utils/response.py
+++ utils/response.py
@@ -10,5 +10,8 @@ class Response(object):
                 pickle.loads(resp_dict["response"])
                 if "response" in resp_dict else
                 None)
+
+            # Check if response is a redirect
+            self.is_redirect = self.status in {301, 302, 303, 307, 308}
         except TypeError:
             self.raw_response = None
