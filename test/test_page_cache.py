0b43e7f214d73e70e9e35020591da71e79529bdc
vctruong100 <vctruong100@gmail.com>
2024-04-21 21:03:58 -0700

    test/test_page_cache: fix test

--- test/test_page_cache.py
+++ test/test_page_cache.py
@@ -2,20 +2,28 @@ import unittest
 import pickle
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse, urlunparse
-from page_cache import parse_response
+from utils.response import Response
+from helpers.page_cache import parse_response
+
+class MockRawResponse:
+    def __init__(self, content, is_redirect=False):
+        self.content = content
+        self.is_redirect = is_redirect
 
 class TestParseResponse(unittest.TestCase):
     def setUp(self):
+        self.url = 'https://www.ics.uci.edu/alumni/stayconnected/index.php'
         self.resp = Response({
-            "url": 'https://www.ics.uci.edu/alumni/stayconnected/index.php',
+            "url": self.url,
             "status": 200,
-            "response": pickle.dumps('<a href="stayconnected/index.php">Link</a>' * 10)
+            "response": pickle.dumps(MockRawResponse('<a href="stayconnected/index.php">Link</a>' * 10))
         })
 
-    def test_parse_response(self):
-        result = parse_response(self.resp)
-        expected_links = {'https://www.ics.uci.edu/alumni/stayconnected/stayconnected/index.php'}
-        self.assertEqual(result.links, expected_links)
+def test_parse_response(self):
+    result = parse_response(self.url, self.resp)
+    print(f'Actual links: {result.links}')  
+    expected_links = {'https://www.ics.uci.edu/alumni/stayconnected/stayconnected/index.php'}
+    self.assertEqual(result.links, expected_links)
 
 if __name__ == '__main__':
     unittest.main()
