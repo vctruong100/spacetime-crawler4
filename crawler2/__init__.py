02bff56ec7ea83bb4fc74377808e1dea663c4379
Jason Vu <jasondv2@uci.edu>
2024-04-22 16:42:13 -0700

    crawler2: Add __init__.py
    This forks the crawler/__init__.py file and changes import references to
    crawler2. The logger name also changes to "crawler2".

---
+++ crawler2/__init__.py
@@ -0,0 +1,31 @@
+# crawler2/__init__.py
+#
+# the crawler interface for "crawler2"
+# the only changes implemented are logger name change and imports
+
+from utils import get_logger
+from crawler2.frontier import Frontier
+from crawler2.worker import Worker
+
+class Crawler(object):
+    def __init__(self, config, restart, frontier_factory=Frontier, worker_factory=Worker):
+        self.config = config
+        self.logger = get_logger("crawler2")
+        self.frontier = frontier_factory(config, restart)
+        self.workers = list()
+        self.worker_factory = worker_factory
+
+    def start_async(self):
+        self.workers = [
+            self.worker_factory(worker_id, self.config, self.frontier)
+            for worker_id in range(self.config.threads_count)]
+        for worker in self.workers:
+            worker.start()
+
+    def start(self):
+        self.start_async()
+        self.join()
+
+    def join(self):
+        for worker in self.workers:
+            worker.join()
