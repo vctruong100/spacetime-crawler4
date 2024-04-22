0a04e840c880695bed9fe462dc0dcfba27745a50
Jason Vu <jasondv2@uci.edu>
2024-04-21 18:03:03 -0700

    crawler2/nurl: Add words, links to attributes

--- crawler2/nurl.py
+++ crawler2/nurl.py
@@ -19,6 +19,9 @@ class Nurl:
     reldepth    The depth relative to its parent URL
                 Depth increases by 1 if the URL is a level
                 below the parent URL. Resets to 0 otherwise.
+
+    words       Tokenized word counts
+    links       Links extracted from this URL
     """
     def __init__(self, url, d=None):
         if d:
@@ -27,10 +30,14 @@ class Nurl:
             self.parent = d["parent"]
             self.absdepth = d["absdepth"]
             self.reldepth = d["reldepth"]
+            self.words = d["words"]
+            self.links = d["links"]
         else:
             self.url = url
             self.status = False
             self.parent = None
             self.absdepth = 0
             self.reldepth = 0
+            self.words = dict()
+            self.links = []
 
