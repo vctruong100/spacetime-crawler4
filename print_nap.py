7f2461eb24288958c7306c3b382e50647d3ac04f
Jason Vu <jasondv2@uci.edu>
2024-04-22 23:30:12 -0700

    print_nap: Add nap file visualizer
    This prints the nap file into stdout. Redirection is necessary if you wish to
    output to a file.

---
+++ print_nap.py
@@ -0,0 +1,39 @@
+# print_nap.py
+#
+# prints the specified nap file to stdout
+
+import sys
+from crawler2.nap import Nap
+
+def main(napfile):
+    nap = Nap(napfile)
+    for k,v in nap.dict.items():
+        print(f"BEGIN HASH {k}")
+        print("-------------------------------")
+        for k2,v2 in v.items():
+            if k2=="words":
+                print(f"{k2}\n")
+                if v2:
+                    for w,c in v2.items():
+                        print(w,c)
+                else:
+                    print("<None>")
+            elif k2 == "links":
+                print(f"{k2}\n")
+                if v2:
+                    for l in v2:
+                        print(l)
+                else:
+                    print("<None>")
+            else:
+                print(f"{k2}\t{v2}")
+            print("-------------------------------")
+        print(f"END HASH {k}")
+        print("\n\n")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: python print_nap.py <napfile>")
+    main(sys.argv[1])
+
