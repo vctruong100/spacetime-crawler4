1e52e93053c7071a926eaef08f18e62609db7d8d
Jason Vu <jasondv2@uci.edu>
2024-04-23 12:01:18 -0700

    crawler2/nap: Increase autosave to 5secs, 200 min writes
    0d8fe26 (crawler2/frontier: Fix loading from intermediate states, 2024-04-23)
    adds several writes to resolve intermediate states and add Nurl objects for
    missing urls, which increases the frequency of autosaves. This increases the
    autosave interval/threshold so that saving happens less frequently.

--- crawler2/nap.py
+++ crawler2/nap.py
@@ -77,14 +77,14 @@ class Nap:
     closed      Whether Nap object was closed
     fname       Nap filename
     writecnt    Write count since last save
-    autosave    Auto-save thread (defaults: 2 seconds, 50 min writes)
+    autosave    Auto-save thread (defaults: 5 seconds, 200 min writes)
     logger      Logger object
 
     dict        Dictionary with actual data
     trans       Dictionary with incoming transactions (NOT IMPLEMENTED)
     mutex       Reentrant lock object on dict
     """
-    def __init__(self, fname, autosave_interval=2, autosave_threshold=50):
+    def __init__(self, fname, autosave_interval=5, autosave_threshold=200):
         self.closed = False
         self.fname = fname
         self.writecnt = 0
