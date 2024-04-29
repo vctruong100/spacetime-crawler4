4597e7d3d29cb56a2450e58791babc79c73db025
Jason Vu <jasondv2@uci.edu>
2024-04-29 12:52:04 -0700

    crawler2/worker: Convert worker thread to a pipeline
    The previous worker thread seems hacked together to support the necessary
    features. This refactors the thread, so it's easier to read and change behavior
    in-between pipelines.
    
    This also tells the crawler to download using requests.get instead of through
    the cache server to test the crawler easily. This should be changed later!

--- crawler2/worker.py
+++ crawler2/worker.py
@@ -6,145 +6,299 @@
 from threading import Thread
 
 from inspect import getsource
-from utils.download import download
 from utils import get_logger
+from utils.download import download
+from utils.response import Response
+
 import scraper2 as scraper
 import time
 
-from helpers.robot_parser import can_fetch, respect_delay
 from helpers.exhash import exhash
-from helpers.simhash import compare_fingerprints
+from helpers.simhash import simhash, compare_fingerprints
+
+from crawler2.nurl import *
+
+
+# worker internal status codes
+E_OK = 0x00
+E_EMPTY = 0x01
+E_AGAIN = 0x02
+
+
+# Downloads are retried up until all delays are exhausted.
+# Each element corresponds to how long the thread should sleep
+# (which means how long the worker thread is stuck on this URL).
+# The amount of delays can be adjusted.
+RETRY_DELAY = [1, 2, 4, 8, 16]
+
+
+# threshold values for exclusion/inclusion
+MIN_CONTENT_LEN = 200
+MAX_CONTENT_LEN = 1000000
+
+MAX_ABSDEPTH = 8
+MAX_RELDEPTH = 1
+MAX_MONODEPTH = 2
+MAX_DUPDEPTH = 0
+
+MIN_WORDS = 200
+
+
+def _assert_no_requests():
+    # basic check for requests in scraper3
+    assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper2.py"
+    assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper2.py"
+
+
+def _fake_response(resp):
+    """Creates a blanket Response object from utils.response.Response
+    """
+    resp2 = Response.__new__()
+    resp2.url = resp.url
+    resp2.status = resp.status_code
+    resp2.raw_response = resp
+    resp2.error = ""
+    return resp2
+
+
+def worker_get_resp(w, nurl, pmut=None, use_cache=True):
+    """Fetches the response of the nurl from the cache server.
+    If `use_cache` is False, it instead downloads using the standard requests.get(...).
+    Downloads shall abide by polite mutexes; it must lock for all domains, and for a specific domain, if it exists.
+    The result is a Response object that matches the interface defined in "utils/response.py".
+
+    Returns a result tuple (ok, err) where:
+        -   ok (1) is an internal status code
+        -   err (2) is the result
+
+    :param w Worker: The worker thread
+    :param pmut PoliteMutex: The domain-specific polite mutex
+    :return: A result tuple
+    :rtype: (int, Any)
+
+    """
+    frontier = w.frontier
+    config = w.config
+    logger = w.logger
+    url = nurl.url
+
+    if not use_cache:
+        # standard download
+        import requests
+        with frontier.dpolmut:
+            if pmut:
+                pmut.lock()
+            resp = requests.get(url)
+            if pmut:
+                pmut.unlock()
+            return (E_OK, _fake_response(resp))
+    else:
+        # from cache server
+        retries = 0
+        resp = None
+        while True:
+            # Download the URL
+            with frontier.dpolmut:
+                if pmut:
+                    pmut.lock()
+                resp = download(url, config, logger)
+                if pmut:
+                    pmut.unlock()
+
+            # If response is not a server error or retries exceeded, stop trying
+            if (resp.status not in range(500, 512) or
+                retries >= len(RETRY_DELAY)):
+                break
+
+            # Wait and increment retries
+            # Worst case scenario: the thread halts indefinitely until its retries are exhausted
+            time.sleep(RETRY_DELAY[retries])
+            retries += 1
+
+        if not resp:
+            # This should not happen
+            return (E_AGAIN, None)
+
+        return (E_OK, resp)
+
+
+def worker_filter_resp_pre(w, nurl, resp):
+    """Filters response before it ever scrapes.
+    This should be called after receiving a response, and is used to avoid unnecessary computations.
+
+    :param w Worker: The worker thread
+    :param nurl Nurl: The nurl from which the response is derived
+    :param resp Response: The Response object
+    :return: Whether response should continue
+    :rtype: bool
+
+    """
+    frontier = w.frontier
+    logger = w.logger
+    nap = frontier.nap
+
+    # Filter response by status
+    # 404 not found, 403 forbidden, 401 unauthorized
+    if resp.status in {401, 403, 404}:
+        nurl.finish = NURL_FINISH_BAD
+        return False
+
+    raw_resp = resp.raw_response
+    raw_content = raw_resp.content
+    raw_content_len = len(raw_resp.content)
+
+    # Content is either too small or too large
+    # Mark as low-info response
+    if raw_content_len < MIN_CONTENT_SIZE or raw_content_len > MAX_CONTENT_SIZE:
+        nurl.finish = NURL_FINISH_LOWINFO_PRE
+        return False
+
+    # Check against exact hashes
+    raw_hash = exhash(raw_content, raw_content_len)
+    nurl.exhash = raw_hash
+
+    with nap.mutex:
+        exact_bucket = nap.exdict.get(raw_hash, None)
+        if exact_bucket:
+            # An intermediate state of a bucket might exist
+            # where the master nurl was not marked as complete, but its bucket exists
+            # Therefore, check against the master nurl before continuing.
+            if exact_bucket[0] == nurl.hash:
+                return True
+
+            # append to existing bucket
+            # mark as too exact response
+            exact_bucket[1].append(nurl.hash)
+            nurl.finish = NURL_FINISH_TOO_EXACT
+            return False
+        else:
+            # make new bucket
+            nap.exdict[raw_hash] = [nurl.hash, []]
+
+    return True
+
+
+def worker_filter_resp_post_text(w, nurl, words):
+    """Filters the response after its text content has been parsed.
+    This should be called after the worker processes the text content of response.
+
+    :param w Worker: The worker thread
+    :param nurl Nurl: The nurl itself
+    :param words dict[str,int]: Word counts
+    :return: Whether response should continue
+    :rtype: bool
+
+    """
+    frontier = w.frontier
+    logger = w.logger
+    nap = frontier.nap
+
+    # Filter response by word counts
+    # If there are too few words, then mark as low-info response
+    if sum(words.values()) < MIN_WORDS:
+        nurl.finish = NURL_FINISH_LOWINFO_POST
+        return False
+
+    # Check against similar hashes
+    raw_hash = simhash(words)
+    nurl.smhash = raw_hash
+
+    with nap.mutex:
+        sim_found = False
+        for key, similar_bucket in nap.smdict.items():
+            if compare_fingerprints(raw_hash, key):
+                # An intermediate state of a bucket might exist
+                # where the master nurl was not marked as complete, but its bucket exists
+                # Therefore, check against the master nurl before continuing.
+                if similar_bucket[0] == nurl.hash:
+                    break
+
+                # append to existing bucket
+                # mark as too similar response
+                similar_bucket[1].append(nurl.hash)
+                nurl.finish = NURL_FINISH_TOO_SIMILAR
+                return False
+            else:
+                # make new bucket
+                nap.smdict[raw_hash] = [nurl.hash, []]
+
+    nurl.words = words
+    return True
+
+
+def worker_sift_urls(w, nurl, scraped_urls):
+    """Sifts through the scraped / extracted URLs, and transforms them to Nurls.
+    This should be called after extracting the URLs from the nurl.
+
+    :param w Worker: The worker thread
+    :param nurl Nurl: The nurl itself
+    :param scraped_urls list[str]: A list of extracted URL strings
+    :return: A list of nurls after it was sifted and transformed
+    :rtype: list[Nurl]
+
+    """
+    logger = w.logger
+    sifted_nurls = []
+
+    # Transform each URLs to nurls and filter before "sifting"
+    for url in scraped_urls:
+        chld = Nurl(url)
+        chld.set_parent(nurl)
+
+        # Filter nurl by depths
+        if (chld.absdepth > MAX_ABSDEPTH or
+            chld.reldepth > MAX_RELDEPTH or
+            chld.monodepth > MAX_MONODEPTH or
+            chld.dupdepth > MAX_DUPDEPTH):
+            continue
+
+        # Append nurl hash to parent nurl links
+        nurl.links.append(chld.hash)
+
+    return sifted_nurls
 
 
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
-        self.logger = get_logger(f"worker2-{worker_id}", "worker")
+        self.logger = get_logger(f"worker-new-{worker_id}", "worker")
         self.config = config
         self.frontier = frontier
-        # basic check for requests in scraper2
-        assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper2.py"
-        assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper2.py"
+        _assert_no_requests()
         super().__init__(daemon=True)
 
 
     def run(self):
         while True:
-            tbd_nurl = self.frontier.get_tbd_nurl()
-            if not tbd_nurl:
-                self.logger.info("Frontier is empty. Stopping Crawler")
-                break
+            # Fetch next nurl / URL
+            nurl = self.frontier.get_tbd_nurl()
 
-            if not can_fetch(tbd_nurl.url):
-                self.logger.info("Access to {tbd_nurl.url} is blocked by robots.txt")
+            # Pipe: get response
+            ok, resp = worker_get_resp(self, nurl, pmut=None, use_cache=False)
+            if ok == E_AGAIN:
+                self.frontier.add_nurl(nurl)
+                continue
+            if ok != E_OK:
                 continue
 
-            respect_delay(tbd_nurl.url)
-
-            # Downloads are retried up until all delays are exhausted.
-            # Each element corresponds to how long the thread should sleep
-            # (which means how long the worker thread is stuck on this nurl).
-            # The amount of delays can be adjusted.
-            retry_delay = [1, 2, 4, 8, 16]
-
-            # Downloads the response
-            # Forces downloads to go through the polite mutex
-            # for downloading any URLs.
-            # Retries based on retry_delay on server errors (5XX codes)
-            resp = None
-            retries = 0
-            while True:
-                with self.frontier.dpolmut:
-                    resp = download(tbd_nurl.url, self.config, self.logger)
-                # Only retry if a server error happened (possibly from the cache server)
-                # or there are remaining delays left
-                if resp.status not in range(500,512) or retries >= len(retry_delay):
-                    break
-                time.sleep(retry_delay[retries])
-                retries += 1
-
-            self.logger.info(
-                f"Downloaded {tbd_nurl.url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
-
-            if not resp:
-                # This shouldn't happen
+            # Pipe: filter response
+            if not worker_filter_resp(self, nurl, resp):
+                self.frontier.mark_nurl_complete(nurl)
                 continue
 
-            raw_resp = resp.raw_response
-            nap = self.frontier.nap
-
-            # Before scraping for nurls, filter the responses first
-            # Scraping is expensive so this is preferable
-            content_length = len(raw_resp.content) # raw_resp.headers.get('Content-Length', 0)
-
-            # Check against content length
-            if content_length < 200 or content_length > 1000000:
-                # Filtered by low information
-                # Page content was too small or too large
-                tbd_nurl.metastr = "!lowinfo"
-                self.frontier.mark_nurl_complete(tbd_nurl)
+            # Pipe: process text content
+            tokens, words = scraper.process_text(resp)
+            if not worker_filter_resp_post_text(self, nurl, resp):
+                self.frontier.mark_nurl_complete(nurl)
                 continue
 
-            # Check against exact hashes
-            exact_hash = exhash(raw_resp.content, content_length)
-            tbd_nurl.exhash = exact_hash
-            with nap.emutex:
-                exact_bucket = nap.exdict.get(exact_hash, None)
+            # Pipe: scrape/extract valid URLs and transform to nurls
+            scraped_urls = scraper.scraper(resp)
+            sifted_nurls = worker_sift_urls(w, nurl, scraped_urls)
 
-                # Not unique, append to bucket
-                if exact_bucket != None:
-                    # Filtered by exact hash
-                    # Mark page as duplicate
-                    # Then add the URL hash to the bucket
-                    # Mark as complete after
-                    tbd_nurl.metastr = "!exact"
-                    exact_bucket[1].append(tbd_nurl.hash)
-                    self.frontier.mark_nurl_complete(tbd_nurl)
-                    continue
-
-                # Unique, add bucket
-                else:
-                    exact_bucket = [tbd_nurl.hash, []]
-                    nap.exdict[exact_hash] = exact_bucket
-
-
-            # Scrape the nurls
-            # Add the nurls
+            # Add nurls to frontier
             # Then mark nurl as complete
-            ok, err = scraper.scraper(tbd_nurl, resp)
-            if ok == scraper.E_OK:
-                scraped_nurls = err
+            for chld in sifted_nurls:
+                self.frontier.add_nurl(chld)
+            self.frontier.mark_nurl_complete(nurl)
 
-                # Resolve similar hashes
-                similar_hash = None
-                with nap.smutex:
-                    for h in nap.smdict.keys():
-                        if compare_fingerprints(tbd_nurl.smhash, h):
-                            # Filtered by similar hash
-                            similar_hash = h
-                            break
 
-                if similar_hash != None:
-                    # Too similar
-                    tbd_nurl.metastr = "!similar"
-                    with nap.smutex:
-                        similar_bucket = nap.smdict.get(similar_hash)
-                        similar_bucket[1].append(tbd_nurl.hash)
-                        self.frontier.mark_nurl_complete(tbd_nurl)
-                        continue # do not scrape further
-                else:
-                    # Unique enough
-                    similar_bucket = [tbd_nurl.hash, []]
-                    with nap.smutex:
-                        nap.smdict[tbd_nurl.smhash] = similar_bucket
-
-                # Add scraped nurls to frontier
-                for scraped_nurl in scraped_nurls:
-                    self.frontier.add_nurl(scraped_nurl)
-
-                self.frontier.mark_nurl_complete(tbd_nurl)
-            else:
-                # Try to resolve scraper error
-                if ok == scraper.E_TEXT_LOWINFO:
-                    tbd_nurl.metastr = "!lowinfo"
-                self.frontier.mark_nurl_complete(tbd_nurl)
