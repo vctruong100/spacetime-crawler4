d98d4f6e768a38967d014e0cbc587087a787268f
Jason Vu <jasondv2@uci.edu>
2024-04-30 18:21:28 -0700

    crawlerman/worker: Use append mode when dumping nurls

--- crawlerman/worker.py
+++ crawlerman/worker.py
@@ -40,7 +40,7 @@ class Worker(Thread):
         self.frontier = frontier
         #_assert_no_requests()
 
-        self.file = open("manf.log", "w")
+        self.file = open("manf.log", "a")
 
         super().__init__(daemon=True)
 
